2a73a87cc89f72bc3953d3332ac3844dd2a60d2a
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-07-27 19:23:19 +0530

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -51,57 +51,59 @@ async def setlang(prog):
 async def carbon_api(e):
  if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
    """ A Wrapper for carbon.now.sh """
-   await e.edit("Processing...")
-   CARBON = 'https://carbon.now.sh/?l={lang}&code={code}'
-   global CARBONLANG
-   textx = await e.get_reply_message()
-   pcode = e.text
-   if pcode[8:]:
-         pcode = str(pcode[8:])
-   elif textx:
-         pcode = str(textx.message) # Importing message to module
-   code = quote_plus(pcode) # Converting to urlencoded
-   url = CARBON.format(code=code, lang=CARBONLANG)
-   chrome_options = Options()
-   chrome_options.add_argument("--headless")
-   chrome_options.binary_location = GOOGLE_CHROME_BIN
-   chrome_options.add_argument("--window-size=1920x1080")
-   chrome_options.add_argument("--disable-dev-shm-usage")
-   chrome_options.add_argument("--no-sandbox")
-   chrome_options.add_argument('--disable-gpu')
-   prefs = {'download.default_directory' : './'}
-   chrome_options.add_experimental_option('prefs', prefs)
-   await e.edit("Processing 30%")
+     if e.fwd_from:
+         return
+     await e.edit("Processing...")
+     CARBON = 'https://carbon.now.sh/?l={lang}&code={code}'
+     global CARBONLANG
+     textx = await e.get_reply_message()
+     pcode = e.text
+        if pcode[8:]:
+           pcode = str(pcode[8:])
+     elif textx:
+           pcode = str(textx.message) # Importing message to module
+     code = quote_plus(pcode) # Converting to urlencoded
+     url = CARBON.format(code=code, lang=CARBONLANG)
+     chrome_options = Options()
+     chrome_options.add_argument("--headless")
+     chrome_options.binary_location = GOOGLE_CHROME_BIN
+     chrome_options.add_argument("--window-size=1920x1080")
+     chrome_options.add_argument("--disable-dev-shm-usage")
+     chrome_options.add_argument("--no-sandbox")
+     chrome_options.add_argument('--disable-gpu')
+     prefs = {'download.default_directory' : './'}
+     chrome_options.add_experimental_option('prefs', prefs)
+     await e.edit("Processing 30%")
 
-   driver = webdriver.Chrome(executable_path=CHROME_DRIVER, options=chrome_options)
-   driver.get(url)
-   download_path = './'
-   driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
-   params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_path}}
-   command_result = driver.execute("send_command", params)
+     driver = webdriver.Chrome(executable_path=CHROME_DRIVER, options=chrome_options)
+     driver.get(url)
+     download_path = './'
+     driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+     params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_path}}
+     command_result = driver.execute("send_command", params)
 
-   driver.find_element_by_xpath("//button[contains(text(),'Export')]").click()
-   sleep(5) # this might take a bit.
-   driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
-   sleep(5)
-   await e.edit("Processing 50%")
-   driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
-   sleep(5) #Waiting for downloading
+     driver.find_element_by_xpath("//button[contains(text(),'Export')]").click()
+     sleep(5) # this might take a bit.
+     driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
+     sleep(5)
+     await e.edit("Processing 50%")
+     driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
+     sleep(5) #Waiting for downloading
 
-   await e.edit("Processing 90%")
-   file = './carbon.png'
-   await e.edit("Done!!")
-   await e.client.send_file(
-         e.chat_id,
-         file,
-         caption="Made using [Carbon](https://carbon.now.sh/about/), a project by [Dawn Labs](https://dawnlabs.io/)",
-         force_document=True,
-         reply_to=e.message.reply_to_msg_id,
-         )
+     await e.edit("Processing 90%")
+     file = './carbon.png'
+     await e.edit("Done!!")
+     await e.client.send_file(
+           e.chat_id,
+           file, photo=open
+           caption="Your File Is Ready",
+           force_document=True,
+           reply_to=e.message.reply_to_msg_id,
+           )
 
-   os.remove('./carbon.png')
-   # Removing carbon.png after uploading
-   await e.delete() # Deleting msg
+     os.remove('./carbon.png')
+     # Removing carbon.png after uploading
+     await e.delete() # Deleting msg
 
 @register(outgoing=True, pattern="^.img (.*)")
 async def img_sampler(event):
