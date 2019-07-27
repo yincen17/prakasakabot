15b29add6e9ac5b64869325b4b1fe34e45e07897
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-07-27 20:21:20 +0530

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -95,7 +95,7 @@ async def carbon_api(e):
         await e.edit("Done!!")
         await e.client.send_file(
               e.chat_id,
-              file, photo=open
+              file,
               caption="Your File Is Ready",
               force_document=True,
               reply_to=e.message.reply_to_msg_id,
