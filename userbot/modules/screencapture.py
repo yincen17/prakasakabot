dd65783033ae553b1287433ee77579215a561ac4
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-07-27 23:49:51 +0530

    Fix

--- userbot/modules/screencapture.py
+++ userbot/modules/screencapture.py
@@ -10,54 +10,54 @@ from userbot import GOOGLE_CHROME_BIN
 
 
 @register(outgoing=True, pattern="^.sc(?: |$)(.*)")
-async screen(capture):
+async captur(shots):
     """ For .sc command, Capture any Site via Url."""
-    if not capture.text[0].isalpha() and capture.text[0] not in ("/", "#", "@", "!"):
-        if capture.fwd_from:
+    if not shots.text[0].isalpha() and shots.text[0] not in ("/", "#", "@", "!"):
+        if shots.fwd_from:
             return
         if GOOGLE_CHROME_BIN is None:
-            await capture.edit("need to install Google Chrome. Module Stopping.")
+            await shots.edit("need to install Google Chrome. Module Stopping.")
             return
-       await capture.edit("Processing ...")
-       start = datetime.now()
-       try:
-           chrome_options = webdriver.ChromeOptions()
-           chrome_options.add_argument('--ignore-certificate-errors')
-           chrome_options.add_argument("--test-type")
-           chrome_options.add_argument("--headless")
-           chrome_options.add_argument("--window-size=1920x1080")
-           # https://stackoverflow.com/a/53073789/4723940
-           chrome_options.add_argument('--no-sandbox')
-           chrome_options.add_argument('--disable-dev-shm-usage')
-           chrome_options.binary_location = GOOGLE_CHROME_BIN
-           await capture.edit("Starting Google Chrome BIN")
-           driver = webdriver.Chrome(chrome_options=chrome_options)
-           input_str = capture.pattern_match.group(1)
-           driver.get(input_str)
-           await capture.edit("Opening web-page")
-           im_png = driver.get_screenshot_as_png()
-           # saves screenshot of entire page
-           driver.close()
-           await capture.edit("Stopping Google Chrome BIN")
-           message_id = event.message.id
-           if capture.reply_to_msg_id:
-               message_id = capture.reply_to_msg_id
-           with io.BytesIO(im_png) as out_file:
-               out_file.name = "screencapture.png"
-               await capture.client.send_file(
-                   capture.chat_id,
-                   out_file,
-                   caption=input_str,
-                   force_document=True,
-                   reply_to=message_id,
-                   allow_cache=False,
-                   silent=True
-               )
-           end = datetime.now()
-           duration = (end - start).seconds
-           await capture.edit(f"Completed screencapture Process in {duration} seconds")
-      except Exception:
-           await capture.edit(traceback.format_exc())
+        await shots.edit("Processing ...")
+        start = datetime.now()
+        try:
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument('--ignore-certificate-errors')
+            chrome_options.add_argument("--test-type")
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--window-size=1920x1080")
+            # https://stackoverflow.com/a/53073789/4723940
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.binary_location = GOOGLE_CHROME_BIN
+            await shots.edit("Starting Google Chrome BIN")
+            driver = webdriver.Chrome(chrome_options=chrome_options)
+            input_str = capture.pattern_match.group(1)
+            driver.get(input_str)
+            await shots.edit("Opening web-page")
+            im_png = driver.get_screenshot_as_png()
+            # saves screenshot of entire page
+            driver.close()
+            await shots.edit("Stopping Google Chrome BIN")
+            message_id = shots.message.id
+            if shots.reply_to_msg_id:
+                message_id = shots.reply_to_msg_id
+            with io.BytesIO(im_png) as out_file:
+                out_file.name = "screencapture.png"
+                await shots.client.send_file(
+                    shots.chat_id,
+                    out_file,
+                    caption=input_str,
+                    force_document=True,
+                    reply_to=message_id,
+                    allow_cache=False,
+                    silent=True
+                )
+            end = datetime.now()
+            duration = (end - start).seconds
+            await shots.edit(f"Completed screencapture Process in {duration} seconds")
+       except Exception:
+            await shots.edit(traceback.format_exc())
            
            
 CMD_HELP.update({
