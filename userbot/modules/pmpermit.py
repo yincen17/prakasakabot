07e842bf973345aae2e6f3c0cdaadb2ac9fa96d0
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-07-30 00:23:40 +0530

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -172,11 +172,13 @@ async def approvepm(apprvpm):
             approve(uid)
         except IntegrityError:
             await apprvpm.edit("`User may already be approved.`")
+            await message.delete()
             return
 
         await apprvpm.edit(
             f"[{name0}](tg://user?id={uid}) `approved to PM!`"
         )
+        await message.delete()
         
         async for message in apprvpm.client.iter_messages(apprvpm.chat_id, from_user='me', search=UNAPPROVED_MSG):
             await message.delete()
