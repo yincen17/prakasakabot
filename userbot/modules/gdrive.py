8e8ef2af4cad3e58091ccab076f76ae50436cc70
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-08-05 11:09:19 +0530

    Update gdrive.py

--- userbot/modules/gdrive.py
+++ userbot/modules/gdrive.py
@@ -312,8 +312,9 @@ async def upload_file(http, file_path, file_name, mime_type, event):
     }
     # Insert a file
     file = drive_service.files().insert(body=body, media_body=media_body).execute()
-    response, status = None
-    while response, status is None:
+    response = None
+    while response is None:
+        status, response = file.next_chunk().execute()
         if status:
             percentage = int(status.progress() * 100)
             progress_str = "[{0}{1}]\nProgress: {2}%\n".format(
