a9810b7ad16b15c641f721bb80132b04ae5a2c64
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-08-05 11:55:28 +0530

    Update gdrive.py

--- userbot/modules/gdrive.py
+++ userbot/modules/gdrive.py
@@ -328,7 +328,7 @@ async def upload_file(http, file_path, file_name, mime_type, event):
     drive_service.permissions().insert(fileId=response.get('id'), body=permissions)
     # Define file instance and get url for download
     file = drive_service.files().get(fileId=response.get('id')).execute()
-    download_url = response.get("webContentLink").execute()
+    download_url = response.get("webContentLink")
     return download_url
 
 @register(pattern="^.gfolder$", outgoing=True)
