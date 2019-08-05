eaf4173fcfc88cdf74745cb6fa24e4856feb4578
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-08-05 22:15:00 +0530

    Update gdrive.py

--- userbot/modules/gdrive.py
+++ userbot/modules/gdrive.py
@@ -5,11 +5,13 @@
 # Licensed under MIT License
 
 import asyncio
+import aiohttp
 import math
 import os
 import time
 from pySmartDL import SmartDL
 from telethon import events
+from telethon.tl.types import DocumentAttributeVideo
 from datetime import datetime
 from apiclient.discovery import build
 from apiclient.http import MediaFileUpload
