aefc2c72206c9fff76052db6eed9be0f05090f00
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-08-06 15:50:48 +0530

    Update chat.py

--- userbot/modules/chat.py
+++ userbot/modules/chat.py
@@ -105,7 +105,7 @@ async def mention(event):
     """ For .chatid, returns the ID of the chat you are in at that moment. """
     if event.fwd_from:
 	return
-    input_str = event.pattern_match.group(1)
+        input_str = event.pattern_match.group(1)
 
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
