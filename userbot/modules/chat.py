db2518af59056f20901f067f513aa56f75645036
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-08-06 15:47:25 +0530

    Update chat.py

--- userbot/modules/chat.py
+++ userbot/modules/chat.py
@@ -103,6 +103,10 @@ async def keep_read(message):
 @register(outgoing=True, pattern="^.mention (.*)")
 async def mention(event):
     """ For .chatid, returns the ID of the chat you are in at that moment. """
+    if event.fwd_from:
+		return
+    input_str = event.pattern_match.group(1)
+
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
     if previous_message.forward:
